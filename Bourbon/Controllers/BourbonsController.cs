2c785015a02f7e88b9c0d577f522f50380b09440
Chloe ONeil <chloesporri@yahoo.com>
2023-03-07 18:14:02 -0800

    add using models and change namespace in bourboncontrollers Co-authored-by: Najma Abdi <anajmabdi@gmail.com> Co-authored-by: Cameron Abel <cameronabel@gmail.com>

--- Bourbon/Controllers/BourbonsController.cs
+++ Bourbon/Controllers/BourbonsController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
-using Bourbon.Models;
+using ConnorBourbon.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
-namespace Bourbon.AddControllers
+namespace ConnorBourbon.Controllers
 {
   public class BourbonsController : Controller
   {
@@ -16,12 +18,15 @@ namespace Bourbon.AddControllers
 
     public ActionResult Index()
     {
-      List<Bourbon> model = _db.Items.ToList();
+      List<Bourbon> model = _db.Bourbons
+                                .Include(bourbon => bourbon.Brand)
+                                .ToList();
       return View(model);
     }
 
     public ActionResult Create()
     {
+      ViewBag.BrandId = new SelectList(_db.Brands, "BrandId", "Name");
       return View();
     }
 
@@ -29,6 +34,44 @@ namespace Bourbon.AddControllers
     public ActionResult Create(Bourbon bourbon)
     {
       _db.Bourbons.Add(bourbon);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Bourbon thisBourbon = _db.Bourbons
+                                .Include(bourbon => bourbon.Brand)
+                                .FirstOrDefault(bourbon => bourbon.BourbonId == id);
+      return View(thisBourbon);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Bourbon thisBourbon = _db.Bourbons.FirstOrDefault(bourbon => bourbon.BourbonId == id);
+      ViewBag.BrandId = new SelectList(_db.Brands, "BrandId", "Name");
+      return View(thisBourbon);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Bourbon bourbon)
+    {
+      _db.Bourbons.Update(bourbon);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Bourbon thisBourbon = _db.Bourbons.FirstOrDefault(bourbon => bourbon.BourbonId == id);
+      return View(thisBourbon);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Bourbon thisBourbon = _db.Bourbons.FirstOrDefault(bourbon => bourbon.BourbonId == id);
+      _db.Bourbons.Remove(thisBourbon);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
