075f1d1b1bcadb54a4e09c72d8cddb855be938d9
Chloe ONeil <chloesporri@yahoo.com>
2023-03-07 18:11:24 -0800

    change namespace in distillery.cs Co-authored-by: Najma Abdi <anajmabdi@gmail.com> Co-authored-by: Cameron Abel <cameronabel@gmail.com>

--- Bourbon/Models/Distillery.cs
+++ Bourbon/Models/Distillery.cs
@@ -1,7 +1,9 @@
-namespace Bourbon.Models;
+namespace ConnorBourbon.Models;
 
 public class Distillery
 {
     public int DistilleryId { get; set; }
     public string Name { get; set; }
+
+    public List<Brand> Brands {get; set;}
 }
