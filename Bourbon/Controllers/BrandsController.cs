f19d5d4fc8b1725d6357de72b9c36b5650f8868c
Chloe ONeil <chloesporri@yahoo.com>
2023-03-07 18:13:29 -0800

    add brands controller Co-authored-by: Najma Abdi <anajmabdi@gmail.com> Co-authored-by: Cameron Abel <cameronabel@gmail.com>

---
+++ Bourbon/Controllers/BrandsController.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+using ConnorBourbon.Models;
+
+namespace ConnorBourbon.Controllers
+{
+  public class BrandsController : Controller
+  {
+    private readonly BourbonContext _db;
+
+    public BrandsController(BourbonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Brand> model = _db.Brands.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Brand brand)
+    {
+      _db.Brands.Add(brand);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+     {
+      Brand thisBrand = _db.Brands
+                                .Include(brand => brand.Bourbons)
+                                .FirstOrDefault(brand => brand.BrandId == id);
+      return View(thisBrand);
+     }
+
+     public ActionResult Edit(int id)
+     {
+      Brand thisBrand = _db.Brands.FirstOrDefault(brand => brand.BrandId == id);
+      return View(thisBrand);
+     }
+
+     [HttpPost]
+     public ActionResult Edit(Brand brand)
+     {
+      _db.Brands.Update(brand);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+     }
+
+     public ActionResult Delete(int id)
+     {
+      Brand thisBrand = _db.Brands.FirstOrDefault(brand => brand.BrandId == id);
+      return View(thisBrand);
+     }
+     [HttpPost, ActionName("Delete")]
+     public ActionResult DeleteConfirmed(int id)
+     {
+      Brand thisBrand = _db.Brands.FirstOrDefault(brand => brand.BrandId == id);
+      _db.Brands.Remove(thisBrand);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+     }
+  }
+}
