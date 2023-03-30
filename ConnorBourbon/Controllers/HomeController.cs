003bcdad5b1b205514ff30669be14056e97eee30
Chloe ONeil <chloesporri@yahoo.com>
2023-03-30 16:33:21 -0700

    add Identity functionality to Home Index route

--- ConnorBourbon/Controllers/HomeController.cs
+++ ConnorBourbon/Controllers/HomeController.cs
@@ -1,11 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
+using ConnorBourbon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace ConnorBourbon.Controllers;
 
 public class HomeController : Controller
 {
-  public ActionResult Index()
+  private readonly ConnorBourbon _db;
+  private readonly UserManager<ApplicationUser> _userManager;
+
+  public HomeController(UserManager<ApplicationUser> userManager, ToDoListContext db)
   {
-    return View();
+    _userManager = userManager;
+    _db = db;
+  }
+
+  [HttpGet("/")]
+  public async Task<ActionResult> Index()
+  {
+    Brands[] bran = _db.Brands.ToArray();
+    Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+    model.Add("brands", bran);
+    string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+    if (currentUser != null)
+    {
+      Bourbon[] bourbons = _db.Bourbons
+                        .Where(entry => entry.User.Id == currentUser.Id)
+                        .ToArray();
+      model.Add("bourbons", bourbons);
+    }
+    return View(model);
   }
 }
+
