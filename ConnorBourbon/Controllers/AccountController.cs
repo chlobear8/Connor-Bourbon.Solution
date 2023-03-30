6453fc2ab4006ae51e024c1679612075afd94a04
Chloe ONeil <chloesporri@yahoo.com>
2023-03-30 16:04:22 -0700

    add Login route to AccountController

--- ConnorBourbon/Controllers/AccountController.cs
+++ ConnorBourbon/Controllers/AccountController.cs
@@ -54,5 +54,32 @@ namespace ConnorBourbon.Controllers
         }
       }
     }
+
+    public ActionResult Login()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      else
+      {
+        Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          ModelState.AddModelError("", "There is something wrong with your email or username. Please try again.");
+          return View(model);
+        }
+      }
+    }
   }
 }
