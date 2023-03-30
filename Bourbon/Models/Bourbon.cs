d390e4beb2e5b7f771cccfe378959baa879ef901
Chloe ONeil <chloesporri@yahoo.com>
2023-03-30 15:57:16 -0700

    add Register View for Account

--- Bourbon/Models/Bourbon.cs
+++
@@ -1,9 +0,0 @@
-namespace ConnorBourbon.Models;
-
-public class Bourbon
-{
-    public int BourbonId { get; set; }
-    public string Name { get; set; }
-    public Brand Brand { get; set; }
-    public int BrandId { get; set; }
-}
--- Bourbon/Models/Distillery.cs
+++
@@ -1,9 +0,0 @@
-namespace ConnorBourbon.Models;
-
-public class Distillery
-{
-    public int DistilleryId { get; set; }
-    public string Name { get; set; }
-
-    public List<Brand> Brands {get; set;}
-}
